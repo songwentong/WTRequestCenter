7b2da82bfa51c39e4cb66debd84a321443d5764f
宋文通 <275712575@qq.com>
2014-08-14 15:26:34 +0800

    添加UIColor的扩展，方便使用

---
+++ WTRequestCenter/UIKit+WTRequestCenter/UIColor+FastCreating.h
@@ -0,0 +1,21 @@
+//
+//  UIColor+FastCreating.h
+//  WTRequestCenter
+//
+//  Created by songwt on 14-8-14.
+//  Copyright (c) 2014年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (FastCreating)
+
+//用色值创建颜色 RGB都是number
++ (UIColor*)WTcolorWithFloat:(CGFloat)number;
+
+//用RGB创建颜色，分别设置RGB，无需除以255
++ (UIColor *)WTcolorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue;
+
+//用RGBA创建颜色，无需除以255.0
++ (UIColor *)WTcolorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha;
+@end
---
+++ WTRequestCenter/UIKit+WTRequestCenter/UIColor+FastCreating.m
@@ -0,0 +1,27 @@
+//
+//  UIColor+FastCreating.m
+//  WTRequestCenter
+//
+//  Created by songwt on 14-8-14.
+//  Copyright (c) 2014年 song. All rights reserved.
+//
+
+#import "UIColor+FastCreating.h"
+
+@implementation UIColor (FastCreating)
+
++ (UIColor*)WTcolorWithFloat:(CGFloat)number
+{
+    return [self WTcolorWithRed:number green:number blue:number];
+}
+
++ (UIColor *)WTcolorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue{
+    return [self WTcolorWithRed:red green:green blue:blue alpha:1.0];
+}
++ (UIColor *)WTcolorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha
+{
+    UIColor *color = nil;
+    color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:alpha];
+    return color;
+}
+@end
