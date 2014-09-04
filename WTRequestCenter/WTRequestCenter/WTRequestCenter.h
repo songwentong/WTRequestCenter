254a0bca9ab27d544a0f59e781e845de9b094158
宋文通 <275712575@qq.com>
2014-09-04 09:37:01 +0800

    import调整

--- WTRequestCenter/WTRequestCenter/WTRequestCenter.h
+++ WTRequestCenter/WTRequestCenter/WTRequestCenter.h
@@ -21,7 +21,10 @@
  */
 #import <Foundation/Foundation.h>
 #import "WTDataSaver.h"
-
+#if ( ( defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090) || \
+( defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000 ) )
+#import "WTURLSessionManager.h"
+#endif
 @interface WTRequestCenter : NSObject
 
 //请求队列
