922b49994624aaa90d1af652d15746ab25d5fa78
宋文通 <275712575@qq.com>
2014-10-29 09:50:50 +0800

    增加注释

--- WTRequestCenter/UIKit+WTRequestCenter/WTNetworkActivityIndicatorManager.h
+++ WTRequestCenter/UIKit+WTRequestCenter/WTNetworkActivityIndicatorManager.h
@@ -4,15 +4,16 @@
 //
 //  Created by song on 14/10/24.
 //  Copyright (c) 2014年 song. All rights reserved.
-//
+
 
 #import <Foundation/Foundation.h>
 
+
+
+//  网络访问指示器，enable设置为YES后网络访问在状态栏有提示
 @interface WTNetworkActivityIndicatorManager : NSObject
 
-
 @property (nonatomic, assign, getter = isEnabled) BOOL enabled;
-
 
 @property (readonly, nonatomic, assign) BOOL isNetworkActivityIndicatorVisible;
 
