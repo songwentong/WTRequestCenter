1465eb39b6113ea1cd51948f62d289fed4713890
mike <275712575@qq.com>
2014-12-04 10:55:45 +0800

    添加注释

--- WTRequestCenter/WTRequestCenter/WTURLRequestSerialization.h
+++ WTRequestCenter/WTRequestCenter/WTURLRequestSerialization.h
@@ -9,22 +9,47 @@
 #import <Foundation/Foundation.h>
 @protocol WTMultipartFormData;
 @interface WTURLRequestSerialization : NSObject
-//POST请求
+/*!
+ POST请求
+ */
 +(NSMutableURLRequest*)POSTRequestWithURL:(NSString*)url
                                parameters:(NSDictionary*)parameters;
 
 
-//支出数据上传
 
+/*!
+ 支持数据上传的POST请求
+ */
 +(NSMutableURLRequest*)POSTRequestWithURL:(NSString*)url
                                parameters:(NSDictionary*)parameters
                 constructingBodyWithBlock:(void (^)(id <WTMultipartFormData> formData))block;
+
 @end
 @protocol WTMultipartFormData
+
+/*!
+    暂未完成
+ */
 - (BOOL)appendPartWithFileURL:(NSURL *)fileURL
                          name:(NSString *)name
                         error:(NSError * __autoreleasing *)error;
 
+/*!
+    数据上传协议
+ */
 - (BOOL)appendPartWithData:(NSData*)data
                       name:(NSString*)name;
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
