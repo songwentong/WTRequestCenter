048dc80fea5f9c2cbdfc0bd975f0125935da29a0
宋文通 <275712575@qq.com>
2014-12-11 14:19:09 +0800

    加上UIApplication的扩展

---
+++ WTRequestCenter/UIKit+WTRequestCenter/UIApplication+WTRequestCenter.h
@@ -0,0 +1,13 @@
+//
+//  UIApplication+WTRequestCenter.h
+//  WTRequestCenter
+//
+//  Created by SongWentong on 14/12/11.
+//  Copyright (c) 2014年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (WTRequestCenter)
+
+@end
---
+++ WTRequestCenter/UIKit+WTRequestCenter/UIApplication+WTRequestCenter.m
@@ -0,0 +1,29 @@
+//
+//  UIApplication+WTRequestCenter.m
+//  WTRequestCenter
+//
+//  Created by SongWentong on 14/12/11.
+//  Copyright (c) 2014年 song. All rights reserved.
+//
+
+#import "UIApplication+WTRequestCenter.h"
+
+@implementation UIApplication (WTRequestCenter)
+
+
+
+
+
+
+//版本号
++(NSString*)WTAppVersion
+{
+    return @"";
+}
+
+//build 号
++(NSString*)WTAppBuild
+{
+    return @"";
+}
+@end
