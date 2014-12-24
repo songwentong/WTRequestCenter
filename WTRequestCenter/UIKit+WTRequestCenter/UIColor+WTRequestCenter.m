b6396594a919e9ddb78acb38f8a5deba1071b913
宋文通 <275712575@qq.com>
2014-12-24 15:27:36 +0800

    简化方法

--- WTRequestCenter/UIKit+WTRequestCenter/UIColor+WTRequestCenter.m
+++ WTRequestCenter/UIKit+WTRequestCenter/UIColor+WTRequestCenter.m
@@ -35,20 +35,7 @@
 
 + (UIColor *)WTcolorWithHexString:(NSString *)str
 {
-    if (!str || [str isEqualToString:@""]) {
-        return nil;
-    }
-    unsigned red,green,blue;
-    NSRange range;
-    range.length = 2;
-    range.location = 1;
-    [[NSScanner scannerWithString:[str substringWithRange:range]] scanHexInt:&red];
-    range.location = 3;
-    [[NSScanner scannerWithString:[str substringWithRange:range]] scanHexInt:&green];
-    range.location = 5;
-    [[NSScanner scannerWithString:[str substringWithRange:range]] scanHexInt:&blue];
-    UIColor *color= [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1];
-    return color;
+    return [self WTcolorWithHexString:str alpha:1.0];
 }
 
 + (UIColor *)WTcolorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha {
