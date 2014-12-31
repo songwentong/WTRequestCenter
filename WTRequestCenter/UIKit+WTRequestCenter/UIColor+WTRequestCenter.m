a6745592a79a6952ea0627953da4c227fdf68d02
宋文通 <275712575@qq.com>
2014-12-31 16:33:08 +0800

    格式优化

--- WTRequestCenter/UIKit+WTRequestCenter/UIColor+WTRequestCenter.m
+++ WTRequestCenter/UIKit+WTRequestCenter/UIColor+WTRequestCenter.m
@@ -12,30 +12,42 @@
 
 + (UIColor*)WTcolorWithFloat:(CGFloat)number
 {
-    return [self WTcolorWithRed:number green:number blue:number];
+    return [self WTcolorWithRed:number
+                          green:number
+                           blue:number];
 }
 
 + (UIColor *)WTcolorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue{
-    return [self WTcolorWithRed:red green:green blue:blue alpha:1.0];
+    return [self WTcolorWithRed:red
+                          green:green
+                           blue:blue
+                          alpha:1.0];
 }
+
 + (UIColor *)WTcolorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha
 {
     UIColor *color = nil;
-    color = [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:alpha];
+    color = [UIColor colorWithRed:red/255.0
+                            green:green/255.0
+                             blue:blue/255.0
+                            alpha:alpha];
     return color;
 }
 
 
 + (NSUInteger)integerValueFromHexString:(NSString *)hexString {
+    
     NSScanner *scanner = [NSScanner scannerWithString:hexString];
     unsigned int result;
     [scanner scanHexInt:&result];
     return result;
+    
 }
 
 + (UIColor *)WTcolorWithHexString:(NSString *)str
 {
-    return [self WTcolorWithHexString:str alpha:1.0];
+    return [self WTcolorWithHexString:str
+                                alpha:1.0];
 }
 
 + (UIColor *)WTcolorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha {
@@ -58,8 +70,11 @@
     }
     
     CGFloat red = [self integerValueFromHexString:[hexString substringWithRange:NSMakeRange(0, 2)]];
+    
     CGFloat green = [self integerValueFromHexString:[hexString substringWithRange:NSMakeRange(2, 2)]];
+    
     CGFloat blue = [self integerValueFromHexString:[hexString substringWithRange:NSMakeRange(4, 2)]];
+    
     return [self WTcolorWithRed:red
                           green:green
                            blue:blue
