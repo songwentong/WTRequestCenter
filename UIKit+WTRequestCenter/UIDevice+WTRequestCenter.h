637ba1d0b63334ae9e1a20095182cd68e99fd442
宋文通 <275712575@qq.com>
2015-01-20 15:49:49 +0800

    调整注释

--- UIKit+WTRequestCenter/UIDevice+WTRequestCenter.h
+++ UIKit+WTRequestCenter/UIDevice+WTRequestCenter.h
@@ -13,12 +13,21 @@
 
 
 //UUID  唯一标示符
+/*!
+ 唯一标示符
+ 注意：如果程序被删除了，然后在安装，这个数值会改变。
+      如果想要永久保证这个数字不变，需要使用Keychain来保存这个字符串
+ */
 +(NSString*)WTUUID;
 
 
-//系统版本
+/*!
+ 系统版本
+ */
 +(CGFloat)systemVersion;
 
-//设备类型
+/*!
+ 设备类型
+ */
 +(NSString *)getDeviceType;
 @end
