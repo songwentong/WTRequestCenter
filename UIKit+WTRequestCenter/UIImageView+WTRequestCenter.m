819d02b831c5ec30b4a5c9c1314acfac83163589
songwentong <songwentong@mobimagic.com>
2015-10-27 09:58:47 +0800

    先把imageview的功能给实现

--- UIKit+WTRequestCenter/UIImageView+WTRequestCenter.m
+++ UIKit+WTRequestCenter/UIImageView+WTRequestCenter.m
@@ -6,11 +6,10 @@
 //  Copyright (c) Mike song(mailto:275712575@qq.com). All rights reserved.
 //  site:https://github.com/swtlovewtt/WTRequestCenter
 
-#import "UIImageView+WTRequestCenter.h"
+
 #import <objc/runtime.h>
-#import "UIImage+WTRequestCenter.h"
-
-
+#import "WTNetWork.h"
+@import UIKit;
 @interface UIImageView()
 //@property (nonatomic,strong,readwrite) WTURLRequestOperation *wtImageRequestOperation;
 
@@ -19,34 +18,9 @@
 
 
 //设置图片的Operation
-static const void * const WTImageViewOperationKey = @"WT ImageView Operation Key";
+//static const void * const WTImageViewOperationKey = @"WT ImageView Operation Key";
 
-static const void * const WTHighlightedImageOperationKey = @"WT Highlighted Image Operation Key";
-
--(WTURLRequestOperation*)wtImageRequestOperation
-{
-    WTURLRequestOperation *operation = (WTURLRequestOperation*)objc_getAssociatedObject(self, WTImageViewOperationKey);
-    return operation;
-}
-
--(void)setWtImageRequestOperation:(WTURLRequestOperation *)wtImageRequestOperation
-{
-    objc_setAssociatedObject(self, WTImageViewOperationKey, wtImageRequestOperation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
--(WTURLRequestOperation*)wtHighlightedImageOperation
-{
-    WTURLRequestOperation *operation = (WTURLRequestOperation*)objc_getAssociatedObject(self, WTHighlightedImageOperationKey);
-    return operation;
-
-}
-
-
-
--(void)setWTHighlightedImageOperation:(WTURLRequestOperation *)operation
-{
-    objc_setAssociatedObject(self, WTHighlightedImageOperationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
+//static const void * const WTHighlightedImageOperationKey = @"WT Highlighted Image Operation Key";
 
 
 
@@ -61,64 +35,26 @@ static const void * const WTHighlightedImageOperationKey = @"WT Highlighted Imag
     [self setImageWithURL:url placeholderImage:placeholder finished:nil failed:nil];
 }
 
--(void)setImageWithURL:(NSString *)url placeholderImage:(UIImage *)placeholder finished:(WTRequestFinishedBlock)finished failed:(WTRequestFailedBlock)failed
+-(void)setImageWithURL:(NSString *)url placeholderImage:(UIImage *)placeholder finished:(dispatch_block_t)finished failed:(dispatch_block_t)failed
 {
-    if (self.wtImageRequestOperation) {
-        [self.wtImageRequestOperation cancel];
-        self.wtImageRequestOperation = nil;
-    }
+
     
     [[NSOperationQueue mainQueue] addOperationWithBlock:^
     {
         self.image = placeholder;
     }];
-
-    if (url) {
-        __weak UIImageView *wself    = self;
-        
-        
-        WTURLRequestOperation *operation = nil;
-        
-        operation = [[WTRequestCenter requestCenter] GETUsingCache:url
-                                                        parameters:nil
-                                                          finished:^(WTURLRequestOperation *operation, NSData *data)
-        {
-            [UIImage imageWithData:data complectionHandler:^(UIImage *image) {
-                
-                if (image) {
-                    if (!wself) return;
-                    __strong UIImageView *strongSelf = wself;
-                    
-                    dispatch_block_t block = ^{
-                        strongSelf.image = image;
-                        [strongSelf setNeedsLayout];
-                    };
-                    
-
-                    [[NSOperationQueue mainQueue] addOperations:@[[NSBlockOperation blockOperationWithBlock:block]]
-                                              waitUntilFinished:YES];
-                }
-                
-                
-            }];
-
-            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                if (finished) {
-                    finished(operation.response,operation.responseData);
-                }
-            }];
-
-        } failed:^(WTURLRequestOperation *operation, NSError *error)
-        {
-        
-        }];
-        
-        
-        self.wtImageRequestOperation = operation;
-    }else
+    
+    
+    NSMutableURLRequest *request = [[WTNetWorkManager sharedKit] requestWithMethod:@"GET" URLString:url parameters:nil error:nil];
+    request.cachePolicy = NSURLRequestReturnCacheDataElseLoad;
+    [[[WTNetWorkManager sharedKit].session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error)
     {
-        
-    }
+        UIImage *image = [UIImage imageWithData:data];
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            self.image = image;
+        }];
+    }] resume];
+    
 }
 
 -(void)setHighlightedImageWithURL:(NSString *)url
@@ -132,44 +68,24 @@ static const void * const WTHighlightedImageOperationKey = @"WT Highlighted Imag
     [self setHighlightedImageWithURL:url placeholderImage:placeholderImage];
 }
 
--(void)setHighlightedImageWithURL:(NSString *)url placeholderImage:(UIImage*)placeholderImage finished:(WTRequestFinishedBlock)finished failed:(WTRequestFailedBlock)failed
+-(void)setHighlightedImageWithURL:(NSString *)url placeholderImage:(UIImage*)placeholderImage finished:(dispatch_block_t)finished failed:(dispatch_block_t)failed
 {
-    if (![self wtHighlightedImageOperation]) {
-        [[self wtHighlightedImageOperation] cancel];
-        [self setWTHighlightedImageOperation:nil];
-    }
-    
-    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-        self.highlightedImage = placeholderImage;
-    }];
-    
-    __weak UIImageView *wself = self;
-    
-    WTURLRequestOperation *operation = nil;
-    operation = [[WTRequestCenter requestCenter] GETUsingCache:url
-                                                    parameters:nil
-                                                      finished:^(WTURLRequestOperation *operation, NSData *data)
-    {
-        [UIImage imageWithData:data complectionHandler:^(UIImage *image) {
-            if (!wself) return;
-            __strong UIImageView *strongSelf = wself;
-            
-            dispatch_block_t block = ^{
-                strongSelf.highlightedImage = image;
-                [strongSelf setNeedsLayout];
-            };
-            
-            [[NSOperationQueue mainQueue] addOperations:@[[NSBlockOperation blockOperationWithBlock:block]]
-                                      waitUntilFinished:YES];
 
-        }];
-    }
-                                                        failed:^(WTURLRequestOperation *operation, NSError *error)
-    {
-        
-    }];
-    [self setWTHighlightedImageOperation:operation];
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^
+     {
+         self.highlightedImage = placeholderImage;
+     }];
     
+    
+    NSMutableURLRequest *request = [[WTNetWorkManager sharedKit] requestWithMethod:@"GET" URLString:url parameters:nil error:nil];
+    request.cachePolicy = NSURLRequestReturnCacheDataElseLoad;
+    [[[WTNetWorkManager sharedKit].session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error)
+      {
+          UIImage *image = [UIImage imageWithData:data];
+          [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+              self.highlightedImage = image;
+          }];
+      }] resume];
 }
 
 @end
