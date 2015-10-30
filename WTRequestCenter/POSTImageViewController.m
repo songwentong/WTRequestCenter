34054e8cff1e48942555760eaf6f7afa76dba474
songwentong <songwentong@mobimagic.com>
2015-10-30 16:27:28 +0800

    调整使用,更加人性化

--- WTRequestCenter/POSTImageViewController.m
+++ WTRequestCenter/POSTImageViewController.m
@@ -9,7 +9,10 @@
 #import "POSTImageViewController.h"
 #import "WTNetWork.h"
 @interface POSTImageViewController ()
-
+{
+    BOOL _sending;
+}
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 @end
 
 @implementation POSTImageViewController
@@ -26,33 +29,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
     
+    _statusLabel.text = @"点击图片来发送";
     
-    
-    //对应的文件(图片,视频)的数据
-    UIImage *image = [UIImage imageNamed:@"image.jpg"];
-    NSData *data1 = UIImageJPEGRepresentation(image, 1.0);
-    
-//    文件数据
-    [dict setValue:data1 forKey:@"content"];
-//    服务端接收的key
-    [dict setValue:@"fileContents" forKey:@"name"];
-//    文件名(可不填)
-    [dict setValue:@"image.jpg" forKey:@"filename"];
-//    文件类型(可不填)
-    [dict setValue:@"image/jpeg" forKey:@"contentType"];
-    
-    NSURLRequest *request = [[WTNetWorkManager sharedKit] POSTRequestWithURL:@"http://localhost:9000/cgi-bin/PostIt.py" parameters:nil body:@[dict]];
-    [[[WTNetWorkManager sharedKit].session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        if (error) {
-            NSLog(@"%@",error);
-        }else{
-            NSString *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-            NSLog(@"%@",s);
-        }
+   
+}
+- (IBAction)send:(id)sender {
+    if (_sending) {
         
-    }] resume];
+    }else{
+        _sending = YES;
+        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+        
+        
+        
+        //对应的文件(图片,视频)的数据
+        UIImage *image = [UIImage imageNamed:@"image.jpg"];
+        NSData *data1 = UIImageJPEGRepresentation(image, 1.0);
+        
+        //    文件数据
+        [dict setValue:data1 forKey:@"content"];
+        //    服务端接收的key
+        [dict setValue:@"fileContents" forKey:@"name"];
+        //    文件名(可不填)
+        [dict setValue:@"image.jpg" forKey:@"filename"];
+        //    文件类型(可不填)
+        [dict setValue:@"image/jpeg" forKey:@"contentType"];
+        
+        NSURLRequest *request = [[WTNetWorkManager sharedKit] POSTRequestWithURL:@"http://localhost:9000/cgi-bin/PostIt.py" parameters:nil body:@[dict]];
+        
+        
+        _statusLabel.text = @"发送中";
+        [[[WTNetWorkManager sharedKit].session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            _sending = NO;
+            if (error) {
+//                NSLog(@"%@",error);
+                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                _statusLabel.text = @"失败";
+                }];
+
+            }else{
+                NSString *s = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//                NSLog(@"%@",s);
+                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                    if (s.length==0) {
+                        _statusLabel.text = @"POST 成功";
+                    }else{
+                        _statusLabel.text = @"失败";
+                    }
+                }];
+
+                
+            }
+            
+        }] resume];
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
