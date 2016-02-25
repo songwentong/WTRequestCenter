02190c9bb9c62837188f2bce84cbe1be945d5ee4
songwentong <songwentong@mobimagic.com>
2016-02-25 11:21:42 +0800

    新加了一个数组的扩展

---
+++ UIKit+WTRequestCenter/NSArray+Sort.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Sort.h
+//  Test
+//
+//  Created by SongWentong on 2/22/16.
+//  Copyright © 2016 SongWentong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Sort)
+- (NSArray*)testSortedArrayUsingComparator:(NSComparisonResult(^)(id o1,id o2))comparator;
+@end
---
+++ UIKit+WTRequestCenter/NSArray+Sort.m
@@ -0,0 +1,50 @@
+//
+//  NSArray+Sort.m
+//  Test
+//
+//  Created by SongWentong on 2/22/16.
+//  Copyright © 2016 SongWentong. All rights reserved.
+//
+
+#import "NSArray+Sort.h"
+
+@implementation NSArray (Sort)
+- (NSArray*)testSortedArrayUsingComparator:(NSComparisonResult(^)(id o1,id o2))comparator
+{
+    NSMutableArray *array = [NSMutableArray arrayWithArray:self];
+    
+    
+    [NSArray sortArray:array UsingComparator:comparator];
+    
+    return array;
+}
+
++(void)sortArray:(NSMutableArray*)array UsingComparator:(NSComparisonResult(^)(id o1,id o2))comparator
+{
+    
+    //查到不合适的就交换一下位置
+    [array enumerateObjectsUsingBlock:^(NSString* obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (idx!=array.count-1) {
+            NSString *obj2 = array[idx+1];
+            if (comparator(obj,obj2)==NSOrderedDescending) {
+                [array exchangeObjectAtIndex:idx withObjectAtIndex:idx+1];
+                *stop = YES;
+            }
+        }
+    }];
+    
+    //重新检查一遍,如果有不合适的顺序就重新调用本方法
+    [array enumerateObjectsUsingBlock:^(NSString* obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (idx!=array.count-1) {
+            NSString *obj2 = array[idx+1];
+            if (comparator(obj,obj2)==NSOrderedDescending) {
+                [self sortArray:array UsingComparator:comparator];
+            }
+        }
+    }];
+    
+    
+    
+}
+
+@end
