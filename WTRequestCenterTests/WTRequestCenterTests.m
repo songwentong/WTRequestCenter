4b7eae8220e9dbbc7fe326516b508b0cb517cabd
songwentong <songwentong@mobimagic.com>
2016-02-25 18:05:46 +0800

    注释一下

--- WTRequestCenterTests/WTRequestCenterTests.m
+++ WTRequestCenterTests/WTRequestCenterTests.m
@@ -31,15 +31,16 @@
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     NSMutableArray *array = [NSMutableArray array];
     for (int i=0; i<1000; i++) {
-//        NSInteger integer = random();
-        NSNumber *number = [NSNumber numberWithInteger:(i+10)];
+        NSInteger integer = random();
+        NSNumber *number = [NSNumber numberWithInteger:integer];
         [array addObject:number];
     }
 
     [self measureBlock:^{
         
-        //1000  0.065 sec  1% STDEV
+        //1000  0.072 sec  2% STDEV
         //10000 6.929 sec  4% stdev
+        
         [NSArray WTsortedArray2:array UsingComparator:^NSComparisonResult(NSNumber * obj1, NSNumber * obj2) {
             //顺序正确,不用动
             if ([obj1 integerValue]<[obj2 integerValue]) {
@@ -53,8 +54,11 @@
             }
         }];
         
+        
         /*
-         //0.007   stdev 15%
+         //系统的效率果然牛逼
+         //1000  0.000 sec 12% STDEV
+         //10000 0.007 sec 9% STDEV
         [array sortedArrayUsingComparator:^NSComparisonResult(NSNumber * obj1, NSNumber * obj2) {
             if ([obj1 integerValue]<[obj2 integerValue]) {
                 return NSOrderedAscending;
@@ -62,7 +66,7 @@
                 return NSOrderedDescending;
             }
         }];
-         */
+        */
 
         
 //        NSLog(@"结果是 %@",result);
