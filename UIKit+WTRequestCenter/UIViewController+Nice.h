dda5cbf81964116e7c3c62763b10169b1b999a2e
songwentong <songwentong@mobimagic.com>
2016-03-02 13:48:39 +0800

    添加注释

--- UIKit+WTRequestCenter/UIViewController+Nice.h
+++ UIKit+WTRequestCenter/UIViewController+Nice.h
@@ -18,6 +18,9 @@
 
 //快速创建来自storyboard的控制器
 @interface UIViewController (IBHelper)
+/*!
+    用于创建来自Main storyboard 下storyboard id和本类的名字一样的的控制器
+ */
 +(instancetype)instanceFromIB;
 +(instancetype)instanceFromStoryBoardName:(NSString*)storyBoardName;
 +(instancetype)instanceFromStoryBoard:(UIStoryboard*)sb;
