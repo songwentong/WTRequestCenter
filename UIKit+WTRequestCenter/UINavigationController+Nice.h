5f8e0278ebb3946a0f50a70a5885c6ba570ebe85
songwentong <songwentong@mobimagic.com>
2016-03-10 15:03:01 +0800

    先创建一个导航器,以后再说

---
+++ UIKit+WTRequestCenter/UINavigationController+Nice.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Nice.h
+//  WTRequestCenter
+//
+//  Created by SongWentong on 3/10/16.
+//  Copyright © 2016 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (Nice)
+
+@end
---
+++ UIKit+WTRequestCenter/UINavigationController+Nice.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Nice.m
+//  WTRequestCenter
+//
+//  Created by SongWentong on 3/10/16.
+//  Copyright © 2016 song. All rights reserved.
+//
+
+#import "UINavigationController+Nice.h"
+
+@implementation UINavigationController (Nice)
+
+@end
