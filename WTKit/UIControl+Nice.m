c37edfe2fd98de77044a8b85233c9fa9a02d9b35
songwentong <songwentong@mobimagic.com>
2016-03-22 17:26:37 +0800

    去掉无用代码

--- WTKit/UIControl+Nice.m
+++ WTKit/UIControl+Nice.m
@@ -10,24 +10,6 @@
 #import <objc/runtime.h>
 
 @implementation UIControl (Nice)
--(NSArray*)methodList
-{
-    return @[];
-}
-
--(void)addtargetforControlEventsTouchUpInside:(dispatch_block_t)block{
-    objc_setAssociatedObject(self, "touchUpInside", block, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-
-    [self addTarget:self action:@selector(methodUIControlEventTouchUpInside:) forControlEvents:UIControlEventTouchUpInside];
-    
-}
-
--(void)methodUIControlEventTouchUpInside:(id)sender{
-    dispatch_block_t block = objc_getAssociatedObject(self, "touchUpInside");
-    if (block) {
-        block();
-    }
-}
 
 
 @end
