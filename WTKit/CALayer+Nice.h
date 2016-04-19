b8a1ee214c21291703993167e7cb37d23073ff90
songwentong <songwentong@mobimagic.com>
2016-04-19 16:30:21 +0800

    加上时髦的声明

--- WTKit/CALayer+Nice.h
+++ WTKit/CALayer+Nice.h
@@ -6,10 +6,11 @@
 //  Copyright © 2016 song. All rights reserved.
 //
 
-
+NS_ASSUME_NONNULL_BEGIN
 @import QuartzCore;
 @class CALayer;
 @class UIImage;
 @interface CALayer (Nice)
 - (UIImage *)snapshot;
 @end
+NS_ASSUME_NONNULL_END
