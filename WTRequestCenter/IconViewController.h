6892e48c23b917a35bd0f4ea53a4a906be3f1091
songwentong <songwentong@mobimagic.com>
2016-05-05 10:47:08 +0800

    加上Icon

---
+++ WTRequestCenter/IconViewController.h
@@ -0,0 +1,13 @@
+//
+//  IconViewController.h
+//  WTRequestCenter
+//
+//  Created by SongWentong on 5/5/16.
+//  Copyright © 2016 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IconViewController : UIViewController
+
+@end
