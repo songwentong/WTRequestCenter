16bb198d52465ba976b1ae97afd650d48ff5af0d
songwentong <songwentong@mobimagic.com>
2017-04-18 15:06:43 +0800

    delete trash

---
+++ Example/Example/Base.lproj/WebImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebImageViewController.h
+//  Example
+//
+//  Created by SongWentong on 18/04/2017.
+//  Copyright © 2017 songwentong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebImageViewController : UIViewController
+
+@end
---
+++ Example/Example/Base.lproj/WebImageViewController.m
@@ -0,0 +1,51 @@
+//
+//  WebImageViewController.m
+//  Example
+//
+//  Created by SongWentong on 18/04/2017.
+//  Copyright © 2017 songwentong. All rights reserved.
+//
+
+#import "WebImageViewController.h"
+
+@interface WebImageViewController ()
+
+@end
+
+@implementation WebImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 100;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return nil;
+}
+
+@end
